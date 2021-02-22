8b189f1e0754583e39c32d7c8e4ee79259d1a591
Sandro <SGamrekelashvili@gmail.com>
2021-02-23 00:41:28 +0400

    ddddd

--- src/App.js
+++ src/App.js
@@ -6,33 +6,23 @@ function App() {
 
 
   useEffect(() => {
-    fetch('https://gis.ge/time/2', {
-      mode: 'cors',
-      headers: {
-          'Content-Type': 'application/x-www-form-urlencoded',
-          'Origin': window.location.href
-      },
-      referrerPolicy: 'origin',
-    }).then(res => res.text()).then(data => {
+    fetch('https://gis.ge/time/2'
+    // , {
+    //   mode: 'cors',
+    //   headers: {
+    //       'Content-Type': 'application/x-www-form-urlencoded',
+    //       'Origin': window.location.href
+    //   },
+    //   referrerPolicy: 'origin',
+    // }
+    ).then(res => res.json()).then(data => {
       console.log(data)
       const b = data.time.split("[")
       const c = b[1].split("]")
       const htmlt=c[0]
       sethtml(htmlt)
-    }).catch((err) => {
-      console.log(err)
     })
-    axios.all([
-      axios.get(`/time/2`)
-      .then(res => {
-        const persons = res.data;
-        console.log("AXIO " + persons)
-        const b = persons.time.split("[")
-        const c = b[1].split("]")
-        const htmlt=c[0]
-        sethtml(htmlt)
-      })
-    ])
+
   }, []);
 
   return (
