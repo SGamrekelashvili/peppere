39f8bf57d1bb814d2b75d7e2c178a911c6cbad0d
Sandro <SGamrekelashvili@gmail.com>
2021-02-22 18:11:41 +0400

    asdasd

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
 
 
   useEffect(() => {
-    fetch('/time/2').then(res => res.json()).then(data => {
+    fetch('/time/2').then(res => res.text()).then(data => {
       console.log(data)
       const b = data.time.split("[")
       const c = b[1].split("]")
