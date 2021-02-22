684208397a4b3f134226314661823d04df5f25ae
Sandro <SGamrekelashvili@gmail.com>
2021-02-22 17:44:43 +0400

    adflhasojfasfasgfasg

--- src/App.js
+++ src/App.js
@@ -5,12 +5,12 @@ function App() {
 
 
   useEffect(() => {
-    fetch('/time/2').then(res => res.text()).then(data => {
+    fetch('https://gis.ge/time/2').then(res => res.json()).then(data => {
       console.log(data)
-      // const b = data.time.split("[")
-      // const c = b[1].split("]")
-      // const htmlt=c[0]
-      sethtml(data)
+      const b = data.time.split("[")
+      const c = b[1].split("]")
+      const htmlt=c[0]
+      sethtml(htmlt)
     })
   }, []);
 
