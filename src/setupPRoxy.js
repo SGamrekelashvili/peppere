bf92e0610535d00bd118d549763978eea032baaa
Sandro <SGamrekelashvili@gmail.com>
2021-02-22 23:55:52 +0400

    s

--- src/setupPRoxy.js
+++ src/setupPRoxy.js
@@ -3,7 +3,7 @@ const proxy = require("http-proxy-middleware")
 module.exports = function(app) {
     app.use(
         proxy("/time/2",{
-            target:"http://46.101.137.243/",
+            target:"http://46.101.137.243",
             secure:false,
             changeOrigin : true
         })
