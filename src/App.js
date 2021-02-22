ab25c6ef68893c080acfc832d744c91504a1101b
Sandro <SGamrekelashvili@gmail.com>
2021-02-22 13:02:45 +0400

    second

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React,{useState} from "React";
+
+
+
+export default function App() {
+  const [html, sethtml] = useState()
+    handleClick()
+
+    handleClick() {
+      console.log('Click happened');
+    }
+
+  return (
+    <>
+      asdasdas
+    </>
+  );
+}
+
